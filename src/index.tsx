ae83ab277caf9728716cc219a1c1448ee8aafa5a
ren15 <ikurbanov015@gmail.com>
2021-07-04 16:42:23 +0300

    added Google OAuth: logIn, logOut

--- src/index.tsx
+++ src/index.tsx
@@ -3,25 +3,45 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-import { Home, Host, Listing, Listings, NotFound, User } from './sections';
+import { Affix, Layout } from 'antd';
+import { AppHeader, Home, Host, Listing, Listings, NotFound, User, Login } from './sections';
 import reportWebVitals from './reportWebVitals';
 import './styles/index.css';
+import { Viewer } from './lib/types';
+import { useState } from 'react';
 
 const client = new ApolloClient({
   uri: '/api',
 });
 
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false,
+};
+
 const App = () => {
+  const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  console.log(viewer);
+
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/host" component={Host} />
-        <Route exact path="/listing/:id" component={Listing} />
-        <Route exact path="/listings/:location?" component={Listings} />
-        <Route exact path="/user/:id" component={User} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout id="app">
+        <Affix offsetTop={0} className="app__affix-header">
+          <AppHeader viewer={viewer} setViewer={setViewer} />
+        </Affix>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/host" component={Host} />
+          <Route exact path="/listing/:id" component={Listing} />
+          <Route exact path="/listings/:location?" component={Listings} />
+          <Route exact path="/login" render={props => <Login {...props} setViewer={setViewer} />} />
+          <Route exact path="/user/:id" component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </Router>
   );
 };
---
+++ src/sections/AppHeader/components/MenuItems/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Avatar, Button, Menu } from 'antd';
+import { HomeOutlined, UserOutlined, LogoutOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/react-hooks';
+import { Viewer } from '../../../../lib/types';
+import { LOG_OUT } from '../../../../lib/graphql/mutations';
+import { LogOut as LogOutData } from '../../../../lib/graphql/mutations/LogOut/__generated__/LogOut';
+import { displayErrorMessage, displaySuccessNotification } from '../../../../lib/utils';
+
+interface Props {
+  viewer: Viewer;
+  setViewer: (viewer: Viewer) => void;
+}
+
+const { Item, SubMenu } = Menu;
+
+export const MenuItem = ({ viewer, setViewer }: Props) => {
+  const [logOut] = useMutation<LogOutData>(LOG_OUT, {
+    onCompleted: data => {
+      if (data && data.logOut) {
+        setViewer(data.logOut);
+        displaySuccessNotification("You've successfully logged out!");
+      }
+    },
+    onError: () => {
+      displayErrorMessage("Sorry! We weren't able to log you out. Please try again later!");
+    },
+  });
+
+  const handleLogOut = () => {
+    logOut();
+  };
+
+  const subMenuLogin =
+    viewer.id && viewer.avatar ? (
+      <SubMenu key="submenu" title={<Avatar src={viewer.avatar} />}>
+        <Item key="/user" icon={<UserOutlined />}>
+          <Link to={`/user/${viewer.id}`}>Profile</Link>
+        </Item>
+        <Item key="/logout" icon={<LogoutOutlined />} onClick={handleLogOut}>
+          Log out
+        </Item>
+      </SubMenu>
+    ) : (
+      <Item key="/login">
+        <Link to="/login">
+          <Button type="primary">Sign In</Button>
+        </Link>
+      </Item>
+    );
+
+  return (
+    <Menu mode="horizontal" selectable={false} className="menu">
+      <Item key="/host" icon={<HomeOutlined />}>
+        <Link to="/host">Host</Link>
+      </Item>
+      {subMenuLogin}
+    </Menu>
+  );
+};
